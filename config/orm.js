bb0d00963cf733787b27ebdb24a0c17f838644bf
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-20 18:04:09 -0400

    rename orm functions to align with assignment requirements for naming

--- config/orm.js
+++ config/orm.js
@@ -34,7 +34,7 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
+  selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
@@ -43,7 +43,7 @@ var orm = {
       cb(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+  insertOne: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -64,7 +64,7 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: Double Bacon, devoured: true}
-  update: function(table, objColVals, condition, cb) {
+  updateOne: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
