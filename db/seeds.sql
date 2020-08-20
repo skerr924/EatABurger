81163310d854fcf24e84166b42556cc9c84c01bb
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-20 17:41:32 -0400

    seeds data adds 5 default burgers to the menu

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('The Heart Attack');
+INSERT INTO burgers(burger_name) VALUES ('The Impossible');
+INSERT INTO burgers(burger_name) VALUES ('The Turkey Avocado');
+INSERT INTO burgers(burger_name) VALUES ('The Cheesefest');
+INSERT INTO burgers(burger_name) VALUES ("Chef's Special");
