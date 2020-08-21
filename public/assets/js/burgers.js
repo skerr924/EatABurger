4656cf0ab5e5ed9260cb9e135a4e900d93909996
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-21 15:42:19 -0400

    removed faulty css line

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,53 +1,55 @@
 // // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  
-//       // Send the PUT request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "PUT",
-//         data: newSleepState
-//       }).then(
-//         function() {
-//           console.log("changed sleep to", newSleep);
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-  
-//     $(".delete").on("click", function(event) {
-//       var id = $(this).data("id");
-  
-//       // Send the DELETE request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "DELETE",
-//         data: id
-//       }).then(
-//         function() {
-//           console.log("deleted cat");
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-  
-$("#addBurger").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
 
-    var newBurger = {
-      burger_name: $("#burgerName").val().trim(),
-    };
+    $(".devourBtn").on("click", function(event){ 
+        var id = $(this).data("id"); 
+        var newDevour = $(this).data("newdevour"); 
+        var newDevourState = { 
+            devoured: newDevour
+        }; 
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+        $.ajax("/api/burgers"+id, { 
+            type: "PUT", 
+            data: newDevourState
+        }).then(
+            function(){ 
+                console.log("changed devoured state to ", newDevour); 
+                location.reload(); 
+            }
+        );
+    });
+
+    $(".addBurger").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burgerName").val().trim(),
+        };
+
+        //POST request to data
+        $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+        }).then(
+        function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+        }
+        );
+    });
+
+    //add delete option here later 
+    // $("DELETE BUTTON ID/CLASS").on("click", function(event){ 
+    //     var id = $(this).data("id"); 
+
+    //     $.ajax("/api/burgers/" + id, { 
+    //         type: DELETE
+    //     }).then(
+    //         function(){
+    //             console.log("deleted burger", id); 
+    //             location.reload(); 
+    //         }
+    //     );
+    // });
 });
